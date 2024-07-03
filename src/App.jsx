ac86054511c3199f7f4cc5d129ecc493eebf8e52
Diptarka Sarkar <132125206+diptarkasarkar@users.noreply.github.com>
2024-07-03 11:12:43 +0000

    Modify

--- src/App.jsx
+++ src/App.jsx
@@ -24,18 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Diptarka Sarkar",
+  title: "Frontend Developer",
+  email: "diptarkasarkar0@gmail.com",
+  gitHub: "https://github.com/diptarkasarkar",
+  linkedIn: "https://www.linkedin.com/in/diptarkasarkar/",
 };
 
-const primaryColor = "#4E567E";
+const primaryColor = "#6BB77B";
 const secondaryColor = "#D2F1E4";
 
 const App = () => {
